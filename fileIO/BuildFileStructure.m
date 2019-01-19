f7ba73fcfbefea5ace36ac21e9d921816a9e10e1
Jeffrey Moffitt <jeffmoffitt@gmail.com>
2019-01-19 07:41:16 -0500

    Fixed a bug in regexp usage in BuildFileStructure

--- fileIO/BuildFileStructure.m
+++ fileIO/BuildFileStructure.m
@@ -42,15 +42,16 @@ function [parsedFileStruct, parameters] = BuildFileStructure(folderPath, varargi
 % Default variables
 % -------------------------------------------------------------------------
 defaults = cell(0,3);
-defaults(end+1,:) = {'fileExt', 'string', '*'};         % File extension to return
-defaults(end+1,:) = {'delimiters', 'cell', {'_'}};      % Delimiters to use to split
+defaults(end+1,:) = {'fileExt', 'string', '*'}; % File extension to return
+defaults(end+1,:) = {'delimiters', 'cell', {'_'}}; % Delimiters to use to split
 defaults(end+1,:) = {'fieldNames', 'cell', {'field1'}}; % FieldNames
-defaults(end+1,:) = {'fieldConv', 'cell', {@char}};     % Conversion functions
+defaults(end+1,:) = {'fieldConv', 'cell', {@char}}; % Conversion functions
 defaults(end+1,:) = {'appendExtraFields', 'boolean', false}; 
-defaults(end+1,:) = {'excludeFlags', 'cell', {}};       % A cell array of strings that are excluded
-defaults(end+1,:) = {'requireFlag', 'string', ''};      % A string that is required
+defaults(end+1,:) = {'excludeFlags', 'cell', {}}; % A cell array of strings that are excluded
+defaults(end+1,:) = {'requireFlag', 'string', ''}; % A string that is required
 defaults(end+1,:) = {'requireExactMatch','boolean',false};
 defaults(end+1,:) = {'containsDelimiters','positive', []}; % An integer specifying a field that might have internal delimiters
+defaults(end+1,:) = {'regExp', 'string', []}; % A regular expression with tokens that match fieldNames
 
 % -------------------------------------------------------------------------
 % Parse and normalize necessary input
@@ -66,6 +67,13 @@ end
 % Parse variable input
 % -------------------------------------------------------------------------
 parameters = ParseVariableArguments(varargin, defaults, mfilename);
+
+% -------------------------------------------------------------------------
+% Check for field names that will overwrite hard coded field names
+% -------------------------------------------------------------------------
+if any(ismember({'name', 'filePath'}, parameters.fieldNames))
+    error('matlabFunctions:invalidArguments', 'name and filePath are protected names.');
+end
 
 % -------------------------------------------------------------------------
 % Define old functionality of internal delimiters
@@ -94,7 +102,11 @@ end
 % -------------------------------------------------------------------------
 % Find files
 % -------------------------------------------------------------------------
-fileData = dir([folderPath '*' parameters.requireFlag '*.' parameters.fileExt]);
+if ~isempty(parameters.requireFlag)
+    fileData = dir([folderPath '*' parameters.requireFlag '*.' parameters.fileExt]);
+else
+    fileData = dir([folderPath '*.' parameters.fileExt]);
+end
 
 % -------------------------------------------------------------------------
 % Exclude files with any of the excluded strings
@@ -113,48 +125,74 @@ end
 parsedFileStruct = [];
 count = 1;
 for i=1:length(fileData)
-    % Split text
-    [splitText, foundDelimiters] = strsplit(fileData(i).name, parameters.delimiters);
-    
-    % Remove extension
-    splitText = splitText(1:(end-1));
-    foundDelimiters = foundDelimiters(1:(end-1));
-    
-    % Combine final field if appropriate
-    if length(splitText) > length(parameters.fieldNames) && parameters.appendExtraFields
-        % Identify field with internal delimiters and recombine split string
-        combinedString = {};
-        lengthDiff = length(splitText) - length(parameters.fieldNames) + 1;
-        startInd = parameters.containsDelimiters;
-        finishInd = length(splitText) - (length(parameters.fieldNames)- startInd);
-        combinedString(1:2:2*lengthDiff) = splitText(startInd:finishInd);
-        combinedString(2:2:(2*lengthDiff-1)) = foundDelimiters(startInd:(finishInd-1));
+    % Switch on whether or not a regular expression was provided
+    if ~isempty(parameters.regExp)
+        % Apply regular expression and capture token values
+        localStruct = regexp(fileData(i).name, parameters.regExp, 'names');
         
-        % Replace split text with the revised values
-        oldSplitText = splitText;
-        splitText = {};
-        splitText(1:(parameters.containsDelimiters-1)) = oldSplitText(1:(parameters.containsDelimiters-1));
-        splitText{parameters.containsDelimiters} = [combinedString{:}];
-        splitText((parameters.containsDelimiters+1):(length(parameters.fieldNames))) = ...
-            oldSplitText((parameters.containsDelimiters+lengthDiff):end);
-    end
-    
-    if parameters.requireExactMatch
-        splitCondition = length(splitText) == length(parameters.fieldNames);
-    else
-        splitCondition = length(splitText) <= length(parameters.fieldNames);
-    end
-    
-    % Parse split text
-    if splitCondition
-        parsedFileStruct(count).name = fileData(i).name;
-        parsedFileStruct(count).filePath = [folderPath fileData(i).name];
-        for j=1:length(splitText)
-            parsedFileStruct(count).(parameters.fieldNames{j}) = ...
-                parameters.fieldConv{j}(splitText{j});
+        % Handle case in which regexp did not match
+        if isempty(localStruct)
+            continue;
         end
-        parsedFileStruct(count).delimiters = parameters.delimiters;
-        count = count + 1;
+        
+        % Apply field conversions
+        for f=1:length(parameters.fieldNames)
+            if ~isfield(localStruct, parameters.fieldNames{f}); % Add empty field if not found
+                localStruct.(parameters.fieldNames{f}) = [];
+            else
+                localStruct.(parameters.fieldNames{f}) = ...
+                    parameters.fieldConv{f}(localStruct.(parameters.fieldNames{f}));
+            end
+        end
+        localStruct.name = fileData(i).name;
+        localStruct.filePath = [folderPath fileData(i).name];
+        localStruct.regExp = parameters.regExp;
+
+        parsedFileStruct= [parsedFileStruct localStruct];        
+    else
+        % Split text
+        [splitText, foundDelimiters] = strsplit(fileData(i).name, parameters.delimiters);
+
+        % Remove extension
+        splitText = splitText(1:(end-1));
+        foundDelimiters = foundDelimiters(1:(end-1));
+
+        % Combine final field if appropriate
+        if length(splitText) > length(parameters.fieldNames) && parameters.appendExtraFields
+            % Identify field with internal delimiters and recombine split string
+            combinedString = {};
+            lengthDiff = length(splitText) - length(parameters.fieldNames) + 1;
+            startInd = parameters.containsDelimiters;
+            finishInd = length(splitText) - (length(parameters.fieldNames)- startInd);
+            combinedString(1:2:2*lengthDiff) = splitText(startInd:finishInd);
+            combinedString(2:2:(2*lengthDiff-1)) = foundDelimiters(startInd:(finishInd-1));
+
+            % Replace split text with the revised values
+            oldSplitText = splitText;
+            splitText = {};
+            splitText(1:(parameters.containsDelimiters-1)) = oldSplitText(1:(parameters.containsDelimiters-1));
+            splitText{parameters.containsDelimiters} = [combinedString{:}];
+            splitText((parameters.containsDelimiters+1):(length(parameters.fieldNames))) = ...
+                oldSplitText((parameters.containsDelimiters+lengthDiff):end);
+        end
+
+        if parameters.requireExactMatch
+            splitCondition = length(splitText) == length(parameters.fieldNames);
+        else
+            splitCondition = length(splitText) <= length(parameters.fieldNames);
+        end
+
+        % Parse split text
+        if splitCondition
+            parsedFileStruct(count).name = fileData(i).name;
+            parsedFileStruct(count).filePath = [folderPath fileData(i).name];
+            for j=1:length(splitText)
+                parsedFileStruct(count).(parameters.fieldNames{j}) = ...
+                    parameters.fieldConv{j}(splitText{j});
+            end
+            parsedFileStruct(count).delimiters = parameters.delimiters;
+            count = count + 1;
+        end
     end
 end
 
